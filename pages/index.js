679957e81e2a0a6eb8d63a1fbfd3380f3462d80a
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-08-03 04:22:00 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Hulu</title>
+      </Head>
       <h1 className="text-3xl font-bold underline">Hello</h1>
     </div>
   );
